7f32d52fb305d8ffd3514968023dd9ac088f12b3
tmnet1999 <tmnet1999@gmail.com>
2022-07-31 20:30:24 +0900

    initial xcommit

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import 'setimmediate'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages/index'
+
+it('Should render title text', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js + GraphQL')).toBeInTheDocument()
+})
